a3db0cc22b0efd2e6fdd4eebfcae4733e930f27a
jorgehar <jharenasr@unadvirtual.edu.co>
2026-04-21 21:52:01 -0500

    Creación del archivo de trabajo

---
+++ app_sigcsr.py
@@ -0,0 +1,6 @@
+# Grupo_129.
+# Fase 4: Sistema Integral de Gestión de Clientes, Servicios y Reservas.
+# Ingeniería de Sistemas.
+# Autoría propia.
+
+print("Sistema Integral de Gestión de Clientes, Servicios y Reservas")
